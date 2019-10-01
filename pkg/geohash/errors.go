f2cfa79c928dfd5ae80ae93225d6352d2b5b046a
TPaschalis <paschalist0@gmail.com>
2019-10-01 22:59:58 +0300

    Renamed core errors according to github.com/golang/go/wiki/Errors

--- pkg/geohash/errors.go
+++ pkg/geohash/errors.go
@@ -2,8 +2,14 @@ package geohash
 
 import "errors"
 
-var InvalidPointError = errors.New("Invalid Point; Latitude and Longitude should be between [-180, 180] and [-90, 90]")
+// https://github.com/golang/go/wiki/Errors
 
-var InvalidPrecisionError = errors.New("Invalid Precision as input; Precision should be a between [1, 12]")
+var ErrInvalidPoint = errors.New("Invalid Point; Latitude and Longitude should be between [-180, 180] and [-90, 90]")
 
-var ErrorHashExceedsMaxPrecision = errors.New("Invalid Hash; Length exceeds max precision of 12 chars")
+var ErrInvalidPrecision = errors.New("Invalid Precision as input; Precision should be a between [1, 12]")
+
+var ErrHashExceedsMaxPrecision = errors.New("Invalid Hash; Length exceeds max precision of 12 chars")
+
+var ErrInvalidCharacterInHash = errors.New("Invalid Hash; Contains a character not in the base32 charset")
+
+var ErrInvalidHash = errors.New("")
